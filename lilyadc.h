5f576d5b10ea37c883db46343a97a5fcef399465
Matthew Kennedy <matthewkennedy@outlook.com>
2023-08-05 00:53:49 -0700

    fix

--- lilyadc.h
+++ lilyadc.h
@@ -2,13 +2,7 @@
 #include "ch.h"
 #include <hal_rtc.h>
 
-
-
-#define ADC_GRP_VSENSE_NUM_CHANNELS 1
-
 #define VREFINT_CAL_VALUE (*(volatile uint16_t *)0x1FF80078)
-
-static const ADCConversionGroup adc_grp_vsense;
 
 // get vpp only
 uint32_t adc_convert_vsense(void);
